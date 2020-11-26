61c1a2bada08dbc667c7ae99d99d16914266b38d
trunganh98 <trunganh98@users.noreply.github.com>
2020-11-26 22:37:56 +0700

    asm

---
+++ MovieWebSite/MovieWebSite/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieWebSite.Models;
+using MovieWebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+namespace MovieWebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MovieController : Controller
+    {
+        public MovieController(JsonFileMovieServices movieService)
+        {
+            this.MovieService = movieService;
+        }
+
+        public JsonFileMovieServices MovieService { get; }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return MovieService.GetMovies();
+        }
+
+        //[HttpPatch]
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string MovieId,
+            [FromQuery] int Rating)
+        {
+            MovieService.AddRating(MovieId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ MovieWebSite/MovieWebSite/Services/JsonFileMovieServices.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using MovieWebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace MovieWebSite.Services
+{
+    public class JsonFileMovieServices
+    {
+        public JsonFileMovieServices(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Movie.json"); }
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Movie[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public void AddRating(string movieId, int rating)
+        {
+            var movies = GetMovies();
+
+            //LINQ
+            var query = movies.First(x => x.Id == movieId);
+
+            if (query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Movie>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    movies
+                    );
+            }
+        }
+    }
+}
